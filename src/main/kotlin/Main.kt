b37c8eb0d1f08996d5926f074a1d9c304d042b4f
Roman Mesler <pay2mesler@gmail.com>
2024-10-01 09:16:20 +0500

    second4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,41 +26,34 @@ fun init() {
     File(gitDir, "HEAD").writeText("ref: refs/heads/master\n")
     println("Initialized git directory")
 }
-
 fun catFile(args: Array<String>) {
-    if (args.size != 3 || args[1] != "-p") {
-        println("Usage ${args[0]} -p <blob_sha>")
+    if(args.size != 3 || args[1] != "-p") {
+        println("Usage: ${args[0]} -p <blob_sha>")
         exitProcess(1)
     }
-
     val objectFile = Path(
         ".git",
         "objects",
         args[2].substring(0, 2),
         args[2].substring(2)
     ).toFile()
-
     val decompressed = decompress(objectFile)
-    val stripped = decompressed.dropWhile { it != '\u0000' }.drop(1).trimEnd()
-    println(stripped)
+    val stripped = decompressed.dropWhile { it != '\u0000' }.drop(1)
+    print(stripped)
 }
-
+fun fail(command: String) {
+    println("Unknown command: $command")
+    exitProcess(1)
+}
 fun decompress(file: File): String {
     val rawBytes = file.inputStream().readAllBytes()
     val buffer = ByteArray(4096)
     val stream = ByteArrayOutputStream()
-
     val inflater = Inflater().apply { setInput(rawBytes) }
     while (!inflater.finished()) {
         val len = inflater.inflate(buffer)
         stream.write(buffer, 0, len)
     }
     inflater.end()
-
     return String(stream.toByteArray(), Charset.defaultCharset())
-}
-
-fun fail(command: String) {
-    println("Unknown command: $command")
-    exitProcess(1)
 }
